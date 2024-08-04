3ef333452889afce7ecf58995d60e1fc73448483
Felipesjc17 <felipe_sjc17@hotmail.com>
2024-08-04 01:40:13 -0300

    Ajuste base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/asset-manager',
+  base: '/asset-manager-ui',
   plugins: [
     vue(),
     Components({
