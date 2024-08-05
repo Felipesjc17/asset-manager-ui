efa22ab33f44f74c32d2113bb1135f07c6a64378
Felipesjc17 <felipe_sjc17@hotmail.com>
2024-08-04 21:47:29 -0300

    Ajuste url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CompoundInterest from '@/modules/compound-interest/CompoundInterest.vue'
-// import HomeComponent from '@/modules/home/HomeComponent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: '',
+      redirect: 'compound-interest'
+    },
+    {
+      path: '/asset-manager-ui/compound-interest',
       name: '',
       redirect: 'compound-interest'
     },
